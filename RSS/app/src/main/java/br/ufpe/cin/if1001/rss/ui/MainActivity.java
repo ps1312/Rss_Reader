1c10a9d79b41c5e6f7f8f730c2e00cac3934d48d
ps1312 <ps1312@gmail.com>
2018-04-11 14:37:24 -0300

    Passo 16 concluido

--- RSS/app/src/main/java/br/ufpe/cin/if1001/rss/ui/MainActivity.java
+++ RSS/app/src/main/java/br/ufpe/cin/if1001/rss/ui/MainActivity.java
@@ -12,6 +12,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.support.v4.content.LocalBroadcastManager;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
